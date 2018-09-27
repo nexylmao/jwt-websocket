c908628ae9401a3b533873af4580245538af712f
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-09-27 06:34:57 +0200

    Structure OnMessageHandler, still can't sit down and work

--- JwtWebSocket/OnMessageHandler.cs
+++ JwtWebSocket/OnMessageHandler.cs
@@ -5,8 +5,15 @@ namespace JwtWebSocket
 {
     public class OnMessageHandler
     {
+        private Dictionary<string, Type> types;
+        
         private Dictionary<string, EventHandler> events;
 
+        public void SignTag(string tag, Type dataType)
+        {
+            
+        }
+        
         public void Handle(string json)
         {
             int indexStart = json.IndexOf("\"tag\":\"");
