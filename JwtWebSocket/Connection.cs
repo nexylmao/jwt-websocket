695c7d037805f78ec2c1b21fc5920a4a9c61b1be
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-09-29 19:30:04 +0200

    Pretty done. To test right now.

--- JwtWebSocket/Connection.cs
+++ JwtWebSocket/Connection.cs
@@ -1,20 +1,148 @@
+using System;
 using JWT;
 using JWT.Algorithms;
+using Newtonsoft.Json;
 using WebSocketSharp;
 
 namespace JwtWebSocket
 {
     public class Connection
     {
+        private string path;
+        private string secret;
         private WebSocket client; // = new WebSocket(*path*);
 
         private IJwtAlgorithm algorithm;
         private IJsonSerializer serializer; // to be JwtWebSocket.JsonParser
         private IBase64UrlEncoder urlEncoder;
-        private IJwtEncoder _encoder; // = new JwtEncoder(algorithm, serializer, urlEncoder);
+        private IJwtEncoder encoder; // = new JwtEncoder(algorithm, serializer, urlEncoder);
 
         private IDateTimeProvider provider;
         private IJwtValidator validator; // = new JwtValidator(serializer, provider);
         private IJwtDecoder decoder; // = new JwtDecoder(serializer, validator, urlEncoder);
+
+        private OnMessageHandler messageHandler;
+
+        public OnMessageHandler MessageHandler
+        {
+            get => messageHandler;
+            set => messageHandler = value;
+        }
+
+        public Connection(string path, string secret)
+        {
+            this.path = path;
+            this.secret = secret;
+            algorithm = new HMACSHA256Algorithm();
+            serializer = new JsonParser();
+            urlEncoder = new JwtBase64UrlEncoder();
+            encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            provider = new UtcDateTimeProvider();
+            validator = new JwtValidator(serializer, provider);
+            decoder = new JwtDecoder(serializer, validator, urlEncoder);
+            client = new WebSocket(path);
+
+            client.OnMessage += (sender, args) =>
+            {
+                // 1. part -> args to json if token
+                string message = args.Data;
+                try
+                {
+                    message = Verify(message);
+                }
+                catch (TokenExpiredException)
+                {
+                    throw new Exception(@"Token has expired");
+                }
+                catch (SignatureVerificationException)
+                {
+                    throw new Exception(@"Token can't be verified");
+                }
+                // 2. part -> handle json
+                if (messageHandler == null)
+                {
+                    throw new Exception(@"No handler is defined");
+                }
+                messageHandler.Handle(message);
+            };
+        }
+
+        public void SubscribeOnOpen(EventHandler onOpenEvent)
+        {
+            client.OnOpen += onOpenEvent;
+        }
+
+        public void UnsubscribeOnOpen(EventHandler onOpenEvent)
+        {
+            client.OnOpen -= onOpenEvent;
+        }
+
+        public void SubscribeOnMessage(EventHandler<MessageEventArgs> onMessageEvent)
+        {
+            client.OnMessage += onMessageEvent;
+        }
+
+        public void UnsubscribeOnMessage(EventHandler<MessageEventArgs> onMessageEvent)
+        {
+            client.OnMessage -= onMessageEvent;
+        }
+        
+        public void SubscribeOnEvent(EventHandler<ErrorEventArgs> onErrorEvent)
+        {
+            client.OnError += onErrorEvent;
+        }
+
+        public void UnsubscribeOnEvent(EventHandler<ErrorEventArgs> onErrorEvent)
+        {
+            client.OnError -= onErrorEvent;
+        }
+
+        public void SubscribeOnClose(EventHandler<CloseEventArgs> onCloseEvent)
+        {
+            client.OnClose += onCloseEvent;
+        }
+
+        public void UnsubscribeOnClose(EventHandler<CloseEventArgs> onCloseEvent)
+        {
+            client.OnClose -= onCloseEvent;
+        }
+        
+        public void Send(EventArgs data, bool jwt)
+        {
+            if (client.ReadyState != WebSocketState.Open)
+            {
+                throw new Exception(@"The connection is not open");
+            }
+            string json;
+            if (jwt)
+            {
+                json = Sign(data);
+            }
+            else
+            {
+                json = JsonConvert.SerializeObject(data);
+            }
+            client.Send(json);
+        }
+        
+        public void Start()
+        {
+            client.Connect();
+        }
+
+        public void Close()
+        {
+            client.Close();
+        }
+        
+        private string Sign(object json)
+        {
+            return encoder.Encode(json, secret);
+        }
+
+        private string Verify(string token)
+        {
+            return decoder.Decode(token, secret, true);
+        }
     }
 }
--- JwtWebSocket/OnMessageHandler.cs
+++ JwtWebSocket/OnMessageHandler.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Newtonsoft.Json;
 
 namespace JwtWebSocket
 {
     public class OnMessageHandler
     {
         private Dictionary<string, object> events;
+        private EventHandler<SocketMessage<object>> defaultTag;
+        
+        public OnMessageHandler()
+        {
+            events = new Dictionary<string, object>();
+            defaultTag = Handler;
+        }
+
+        public OnMessageHandler(EventHandler<SocketMessage<object>> defaultTag)
+        {
+            events = new Dictionary<string, object>();
+            this.defaultTag = defaultTag;
+        }
 
         public void SignTag(string tag, Type type)
         {
@@ -14,22 +28,39 @@ namespace JwtWebSocket
             {
                 throw new Exception("Tag already exists");
             }
-
             var sm = typeof(SocketMessage<>).MakeGenericType(type);
             var eh = typeof(EventHandler<>).MakeGenericType(sm);
             ConstructorInfo ctor = eh.GetConstructors()[0];
-
             Action<object, EventArgs> handler = Handler;
-            
             var eventHandler = ctor.Invoke(new object[]{handler});
+            events.Add(tag, eventHandler);
         }
 
-        private void Handler(object arg1, EventArgs arg2) { }
+        public object GetHandler(string tag)
+        {
+            if (!events.ContainsKey(tag))
+            {
+                throw new Exception("Tag doesn\'t exist");
+            }
+            return events[tag];
+        }
+        
+        private static void Handler(object arg1, EventArgs arg2) { }
 
         public void Handle(string json)
         {
             const string tagHead = "\"tag\":\"";
             var indexStart = json.IndexOf(tagHead, StringComparison.Ordinal) + tagHead.Length;
+            var indexEnd = json.IndexOf('"', indexStart);
+            var tag = json.Substring(indexStart, indexEnd - indexStart);
+
+            if (!events.ContainsKey(tag))
+            {
+                defaultTag?.Invoke(this, JsonConvert.DeserializeObject<SocketMessage<object>>(json));
+                return;
+            }
+            dynamic eventHandler = events[tag];
+            eventHandler(this, JsonConvert.DeserializeObject(json));
         }
     }
 }
